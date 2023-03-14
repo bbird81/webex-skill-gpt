c32c9fe729d93679e949739715ef555d257c4f72
Thomas <t.guzzoni@lutech.it>
2023-03-14 23:08:01 +0000

    Funziona con rev-proxy:v0.5

--- startup.sh
+++ startup.sh
@@ -5,4 +5,6 @@ echo
 echo "***********************************"
 echo "---------> PUBLIC KEY <------------"
 cat /lutech/id_rsa.pub
-webex-skills skills run lutech
+#webex-skills skills run lutech
+cd /lutech
+uvicorn --port 8080 --host 0.0.0.0 --reload lutech.main:api
