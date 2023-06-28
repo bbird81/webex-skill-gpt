4d7936b4f50eedc7bf943cf2f94e1240c247f80b
bbird <thomasguz@gmail.com>
2023-06-28 18:31:24 +0200

    added nginx auto-start

--- containers/nginx-reverse-proxy/startup.sh
+++ containers/nginx-reverse-proxy/startup.sh
@@ -3,10 +3,14 @@ if [ -z $FQDN ]; # if FQDN env var is empty
 then
     echo "FQDN env var not set: ABORTING!"
 else
-    echo "*********** FQDN **********"
-    echo "Your server fqdn is $FQDN"
-    echo "***************************"
+    echo "***************** FQDN *****************"
+    echo " Your server fqdn is $FQDN"
+    echo "****************************************"
     sed -i "s/your_server_fqdn/$FQDN/" /etc/nginx/conf.d/default.conf
     echo default.conf file:
+    echo "------------------------------------------------------"
     cat /etc/nginx/conf.d/default.conf
+    echo "------------------------------------------------------"
+    echo "Starting NGINX..."
+    nginx -g 'daemon off;'
 fi
